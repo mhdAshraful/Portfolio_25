32bc921c2584cd2561c56f839890973aab557d0a
Mohammed Ashraful Islam <md_ashraful@icloud.com>
2025-07-06 06:52:08 +0600

    maintanance

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import glsl from "vite-plugin-glsl";
+
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -22,26 +22,6 @@ export default defineConfig({
 	},
 	plugins: [
 		react(),
-		glsl({
-			include: [
-				// Glob pattern, or array of glob patterns to import
-				"**/*.glsl",
-				"**/*.wgsl",
-				"**/*.vert",
-				"**/*.frag",
-				"**/*.vs",
-				"**/*.fs",
-			],
-			// // ".glsl" extension will be added automatically:
-			// example: #include utils/chunk1;
-			exclude: undefined, // Glob pattern, or array of glob patterns to ignore
-			warnDuplicatedImports: true, // Warn if the same chunk was imported multiple times
-			removeDuplicatedImports: false, // Automatically remove an already imported chunk
-			defaultExtension: "glsl", // Shader suffix when no extension is specified
-			minify: false, // Minify/optimize output shader code
-			watch: true, // Recompile shader on change
-			root: "/", // Directory for root imports
-		}),
 	],
 	server: {
 		// To Include a Lygia lighting function from online
